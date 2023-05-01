3919cdd03b19036235fec7a21e4fbc6d580d1b0e
jai <jaijai.stell@gmail.com>
2023-05-01 10:39:45 -0700

    stress

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actors) => (
+        <div key={actors.name}>
+          <h2>{actors.name}</h2>
+          <ul>
+            {actors.movies.map((movie) => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "orange",
+  textDecoration: "none",
+  color: "white",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+
+      <NavLink exact to="/"
+        style={linkStyles}
+        activeStyle={{
+          background: "darkgreen",
+        }}>
+
+        Home
+      </NavLink>
+
+      <NavLink to="/movies"
+        style={linkStyles}
+        activeStyle={{
+          background: "darkgreen",
+        }}>Movies</NavLink>
+
+      <NavLink to="/directors"
+        style={linkStyles}
+        activeStyle={{
+          background: "darkgreen",
+        }}>Directors</NavLink>
+
+      <NavLink to="/actors"
+        style={linkStyles}
+        activeStyle={{
+          background: "darkgreen",
+        }}>Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
